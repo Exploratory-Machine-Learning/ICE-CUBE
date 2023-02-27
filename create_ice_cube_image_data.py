e3641cf35b8b6af31d9450e404b711b2b9cb9d1e
akapoorcern <akapoor@cern.ch>
2023-02-27 20:24:04 +0100

    update to image creation

--- create_ice_cube_image_data.py
+++ create_ice_cube_image_data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class ice_cube_data:
     # everything for one batch only now
-    def __init__(self,fbatch,width={'x':20,'y':20,'z':40},frac=0.1):
+    def __init__(self,fbatch,width={'x':20,'y':20,'z':40}):
         import pandas as pd
         from pyarrow.parquet import ParquetFile
         import pyarrow as pa
@@ -14,9 +14,9 @@ class ice_cube_data:
         self.fmeta=ParquetFile("/scratch/ICE-CUBE/train_meta.parquet") # meta data
         self.fbatch=fbatch # dictionary with all the batches
         
-        self.sensorgeom_df = pd.read_csv(self.fsensor).set_index("sensor_id")# dataframe with sensor geometry
+        self.sensorgeom_df_orig = pd.read_csv(self.fsensor).set_index("sensor_id")# dataframe with sensor geometry
         
-        
+        self.sensorgeom_df=self.sensorgeom_df_orig.copy()
         for var in ['x','y','z']:
             self.sensorgeom_df[var] = ((self.sensorgeom_df[var] - self.sensorgeom_df[var].min()) / 
                                        (self.sensorgeom_df[var].max() - self.sensorgeom_df[var].min()))*width[var]
@@ -29,21 +29,27 @@ class ice_cube_data:
         self.zdim=(self.sensorgeom_df['z'].max()-self.sensorgeom_df['z'].min())+1
         
         self.sensorgeom=self.sensorgeom_df.to_dict()
+        self.sensorgeomorig=self.sensorgeom_df_orig.to_dict()
         
         #charge depost and temporal data and meta data for only 1st batch (for now)
         self.batch= pd.read_parquet(self.fbatch['1']).query("(event_id%100==0) & (auxiliary==False)").drop(["auxiliary"],axis=1)
         #print(self.batch.loc[2800])
         #Just make a charge data, by summing charge deposits on the same sensor for each event, and create sensor_id again as column
-        self.events=self.batch.drop(["time"],axis=1).groupby(["event_id","sensor_id"]).sum("charge").reset_index(level=[1])
+        self.realevents=self.batch.drop(["time"],axis=1).groupby(["event_id","sensor_id"]).sum("charge").reset_index(level=[1])
         
         first_n_rows = next(self.fmeta.iter_batches(batch_size = 185806*100))#
         
         # Create meta data frame
         self.meta=pa.Table.from_batches([first_n_rows]).to_pandas()
         
+        self.events=self.realevents.copy()
         self.events['x']=self.events['sensor_id'].apply(self.getx)
         self.events['y']=self.events['sensor_id'].apply(self.gety)
         self.events['z']=self.events['sensor_id'].apply(self.getz)
+        
+        self.realevents['x']=self.realevents['sensor_id'].apply(self.getxo)
+        self.realevents['y']=self.realevents['sensor_id'].apply(self.getyo)
+        self.realevents['z']=self.realevents['sensor_id'].apply(self.getzo)
         
         #for var in ['x','y','z']:
         #    self.events['x']=self.events[var]-self.events[var].min()
@@ -52,15 +58,20 @@ class ice_cube_data:
         num_events=len(self.events.index.unique().to_list())
         self.img_data=np.zeros((num_events,self.xdim,self.ydim,self.zdim))
         self.labels=np.zeros((num_events,2))
-        #print(f'creating image of shape:{self.img_data.shape}')
+        self.eventdict={}
+        for ievent,indexevent in enumerate(self.events.index.unique().to_list()):
+            self.eventdict[indexevent]=ievent
+
+        myevents=self.events.copy()
+        myevents = myevents.reset_index()
         
-        @jit
-        def runloop():
-            for ievent,indexevent in enumerate(self.events.index.unique().to_list()):
-                for x,y,z,c in self.events.loc[indexevent][["x","y","z","charge"]].values:
-                    self.img_data[int(ievent),int(x),int(y),int(z)]=c
-                    self.labels[int(ievent),0]=list(self.meta.query('event_id=='+str(indexevent))['azimuth'].values)[0]
-                    self.labels[int(ievent),1]=list(self.meta.query('event_id=='+str(indexevent))['zenith'].values)[0]
+        def fill_image_data(row):
+                self.img_data[self.eventdict[int(row.event_id)], int(row.x), int(row.y), int(row.z)] = row.charge
+                self.labels[self.eventdict[row.event_id], 0] = self.meta.loc[ievent, "azimuth"]
+                self.labels[self.eventdict[row.event_id], 1] = self.meta.loc[ievent, "zenith"]
+                
+        myevents.apply(fill_image_data, axis=1)
+        
                 
         #print(f'created an image of shape:{self.img_data.shape} !!')
         return self.img_data,self.labels
@@ -71,6 +82,13 @@ class ice_cube_data:
         return self.sensorgeom['y'][sensorid]
     def getz(self,sensorid):
         return self.sensorgeom['z'][sensorid]
+    
+    def getxo(self,sensorid):
+        return self.sensorgeomorig['x'][sensorid]
+    def getyo(self,sensorid):
+        return self.sensorgeomorig['y'][sensorid]
+    def getzo(self,sensorid):
+        return self.sensorgeomorig['z'][sensorid]
 
         
         
